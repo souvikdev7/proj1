d478b5b389222e3502a55758e23de4f83635a693
souvikdev7 <souvik.stg@gmail.com>
2022-10-28 12:34:43 +0530

    modified

--- app.js
+++ app.js
@@ -1,5 +1,214 @@
 const express = require("express");
 const app = express();
 var port = process.env.PORT || 3000;
+
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+
+const options = {
+  failOnErrors: true, // Whether or not to throw when parsing errors. Defaults to false.
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Hello World',
+      version: '1.0.0',
+    },
+    servers:[{url : "http://localhost:3000/"}],
+   /* securityDefinitions: {
+      bearerAuth: {
+          type: 'apiKey',
+          name: 'Authorization',
+          scheme: 'bearer',
+          in: 'header',
+      },
+    security: [ { bearerAuth: [] } ],  
+  }*/
+  },
+  apis: ['./app.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+
+
+
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          data1:
+ *              type : object
+ *              properties:
+ *                  name:
+ *                      type: string
+ *                  email:
+ *                      type: string
+ *   
+ */
+
+ /**
+ * @swagger
+ *  components:
+ *      securitySchemes:
+ *        bearerAuth:
+ *          type: http
+ *          scheme: bearer
+ *          bearerFormat: JWT
+ *      schemas:
+ *          data2:
+ *              type : object
+ *              properties:
+ *                  a:
+ *                      type: integer
+ *                  b:
+ *                      type: integer
+ *   
+ */
+
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Welcome to swagger
+ *     responses:
+ *       200:
+ *         description: Returns a  string.
+ */
+
 app.get('/',(req,res) => res.send("Hello Souvik"));
+
+
+
+app.get('/users',(req,res) => res.json({"name":"Shyam","email":"shyamjaiswal@gmail.com"}));
+
+
+
+/**
+ * @swagger
+ * /allusers:
+ *   get:
+ *     summary :  yyyyyy
+ *     description: aaa
+ *     responses:
+ *       200:
+ *         description: this is a api
+ *         content:
+ *          application/json:
+ *              schema:
+ *                  type: array
+ *                  items:
+ *                      $ref: '#components/schemas/data1'
+ *                      
+ */
+
+
+
+app.get('/allusers',(req,res) => res.json([{"name":"Shyam","email":"shyamjaiswal@gmail.com"},{"name":"Anil","email":"anil88@gmail.com"}]));
+
+
+/**
+ * @swagger
+ * /allusers1/{id}:
+ *   get:
+ *     summary :  eeeee
+ *     description: hhhhh
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        descreiption: Numeric value
+ *        schema:
+ *          type: integer 
+ *     responses:
+ *       200:
+ *         description: this is a api
+ *         content:
+ *          application/json:
+ *              schema:
+ *                  type: array
+ *                  items:
+ *                      $ref: '#components/schemas/data1'
+ *                      
+ */
+
+app.get('/allusers1/:id',(req,res) => res.json([{"name":"Anil","email":"anil88@gmail.com"}]));
+
+
+
+/**
+ * POST
+ */
+
+ //app.use(bodyParser.json());   // Depricated
+ //app.use(bodyParser.urlencoded({ extended: true }));  // Depricated
+
+ app.use(express.json());
+ app.use(express.urlencoded({extended: true}));
+
+ 
+ /**
+ * @swagger
+ * /sum:
+ *   post:
+ *     security:
+ *      - bearerAuth: []
+ *     summary :  llllll
+ *     description: pppppppp  
+ *     requestBody : 
+ *      content:
+ *          application/json:
+ *              schema:
+ *                  $ref: '#components/schemas/data2'
+ *     responses:
+ *       200:
+ *         description: this is a api
+ *         
+ *                      
+ */
+ app.post('/sum',(req, res) => {
+     let a = req.body.a;
+     let b = req.body.b; 
+     let c = parseInt(a) + parseInt(b);
+     res.json(c);
+     console.log('Result : '+c);
+     console.log(req.headers.authorization); // <-- token
+ });
+
+
+  /**
+ * @swagger
+ * /check/{id}:
+ *   put:
+ *     summary :  dddd
+ *     description: uuuuuu
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        descreiption: Numeric value
+ *        schema:
+ *          type: integer 
+ *     requestBody : 
+ *      content:
+ *          application/json:
+ *              schema:
+ *                  $ref: '#components/schemas/data2'
+ *     responses:
+ *       200:
+ *         description: this is a api
+ *         
+ *                      
+ */
+ app.put('/check/:id',(req, res) => {
+    let a = req.body.a;
+    let b = req.body.b; 
+    let c = 'updated';
+    res.json(c);
+    console.log('Result : '+c);
+});
+
 app.listen(port,()=> console.log("Server running on port"+port));
---
+++ test/test.js
@@ -0,0 +1,132 @@
+var assert = require('assert');
+const { log } = require('console');
+const { devNull } = require('os');
+var expect = require('chai').expect;
+
+//const url = 'https://gorest.co.in/public/v2/';
+//const token  = '17a529e5f18f0396ccb3a3e366efbc14b693107e36ad79b8e26ff61051acab95';
+
+const url = "http://localhost:3000/";
+const token  = "1234";
+var supertest = require('supertest');
+var request= supertest.agent(url);
+
+//const pUrl  = 'users/5';
+const pUrl  = 'users';
+const apiUrl  = pUrl+'?access-token=${token}';
+
+
+
+/**
+ * 
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+*/
+
+
+
+describe('Users',()=>{
+
+
+    it.only('GET/users',()=>{
+        return request.get(apiUrl).then((res)  => {
+            //console.log(err);
+            //onsole.log(res.body);
+            //console.log(res.text);
+
+            //expect(res.body).to.not.be.empty;
+            //expect(res.body).to.be.empty;
+            //expect(res.body).to.be.a('array');
+
+
+
+            
+            let data1 = JSON.parse([res.text]);
+
+             console.log(data1);
+
+
+            //expect(data1).to.not.be.empty;
+            //expect(data1).to.be.empty;
+            //expect(data1).to.have.lengthOf(3);
+            //expect(data1).to.have.lengthOf.above(146);
+            //expect(data1).to.have.lengthOf.at.least(2);
+            //expect(data1).to.have.property('email');
+            //expect(data1).to.have.property('name');            
+            //expect(data1).to.have.any.keys('name');
+            expect(data1).to.have.deep.property('name');
+
+
+            
+            
+        });
+    });
+
+    it('GET/users/:id',()=>{
+      return request.get(apiUrl).then((res)  => {        
+             
+          let data1 = JSON.parse([res.text]);
+           //console.log(data1.email);         
+          expect(data1).to.have.any.keys('name');          
+          
+      });
+    });
+
+
+    
+    it('POST/sum',()=>{
+      let myjson = {"a":10,"b":20};
+      return request
+        .post('sum')
+        .set("Authorization","Bearer ${token}")
+        .send(myjson)
+        .then((res)  => {        
+             
+         let data1 = parseInt(res.text);      
+         
+         //expect(data1).to.eq(30);       
+         //expect(data1).to.eq(3);      
+         //expect(data1).to.be.true;
+         //expect(data1).to.not.be.undefined; 
+         //expect(data1).to.be.above(10); 
+         expect(data1).to.be.below(50); 
+        });
+    });
+    /*
+    it('PUT/check',()=>{
+      let myjson = {"a":2,"b":5};
+      return request
+        .put('check')
+        .set("Authorization","Bearer ${token}")
+        .send(myjson)
+        .then((res)  => {        
+             
+         let data1 = parseInt(res.text);      
+     
+         expect(data1).to.not.be.undefined; 
+       
+        });
+    });*/
+
+
+
+    it.only('PUT/check', async ()=>{
+      let myjson = {"a":2,"b":5};
+      let ret2 = await request.put('check').set("Authorization","Bearer ${token}").send(myjson);
+
+        expect(ret2.text).to.be.undefined;   
+       
+    });
+
+});
+
+ 
+
+
+Run a Collection
+https://www.youtube.com/watch?v=8UR1NzuT7jg
